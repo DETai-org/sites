ceb323e30062b57a22cefc780f39910d198ceaf4
Anton-Psy <a17527905@gmail.com>
2026-01-07 01:50:48 +0300

    Зафиксировать файлы блога для сборки

--- personal-site/next.config.js
+++ personal-site/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  outputFileTracingIncludes: {
+    "/app/blog/[slug]/page": ["./lib/blog/posts/**"],
+    "/app/api/images/posts/[slug]/route": ["./wordpress-data/uploads/**"]
+  }
 };
 
 module.exports = nextConfig;
