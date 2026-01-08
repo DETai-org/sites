7dc5e2b559daf6ac0d536d378a265157cea94db7
Anton-Psy <a17527905@gmail.com>
2026-01-08 17:30:23 +0300

    Remove Sanity Studio from personal-site

--- personal-site/lib/blog/blog.utils.ts
+++ personal-site/lib/blog/blog.utils.ts
@@ -1,31 +1,12 @@
 import { getBlogPosts } from "./blog.data";
 import { BlogPost } from "./types";
 import { buildBlogPostDescription } from "./blog.description";
-import {
-  getSanityBlogPostBySlug,
-  getSanityBlogPosts,
-  isSanityConfigured,
-} from "./blog.sanity";
 
 export async function getAllBlogPosts(): Promise<BlogPost[]> {
-  if (isSanityConfigured()) {
-    const posts = await getSanityBlogPosts();
-    if (posts.length) {
-      return posts;
-    }
-  }
-
   return getBlogPosts();
 }
 
 export async function getBlogPostBySlug(slug: string): Promise<BlogPost | undefined> {
-  if (isSanityConfigured()) {
-    const post = await getSanityBlogPostBySlug(slug);
-    if (post) {
-      return post;
-    }
-  }
-
   const posts = await getBlogPosts();
   return posts.find((post) => post.slug === slug);
 }
