0eaa1400bc4ca517f7fcaf44a3dba630e9cfa6db
Anton-Psy <a17527905@gmail.com>
2026-01-21 20:33:50 +0300

    Update blog.base.ts

--- detai-site/lib/blog/blog.base.ts
+++ detai-site/lib/blog/blog.base.ts
@@ -14,7 +14,7 @@ export const baseBlogPosts: BlogPostBase[] = [
     keywords: [{ slug: "keyword:inner-tension" }],
     keywordsRaw: ["успех", "преодоление", "мотивация"],
     coverImage: {
-      src: "/images/posts/maxresdefault.jpg",
+      src: "personal-site/public/images/posts/maxresdefault.jpg",
       width: 1280,
       height: 720,
       alt: "Эта история про одного человека",
